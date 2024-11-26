520c7259198113a34b8080b08c3aee21f89ae351
sanskarkalal <skalal2@uic.edu>
2024-11-26 15:20:04 -0600

    o

--- Server/src/main/java/ServerController.java
+++ Server/src/main/java/ServerController.java
@@ -29,7 +29,8 @@ public class ServerController {
     private Stage stage;
     private Scene introScene;
     private Scene gameInfoScene;
-
+    ArrayList<ClientThread> clientThreads = new ArrayList<>();
+    int count = 0;
 
     public void setStageAndScenes(Stage stage, Scene introScene, Scene gameInfoScene) {
         this.stage = stage;
@@ -40,17 +41,19 @@ public class ServerController {
     @FXML
     public void startServer() throws IOException {
 
-        ServerSocket mysocket = new ServerSocket(Integer.parseInt(portField.getText()));
-        System.out.println("Server is waiting for a client!");
-        stage.setScene(gameInfoScene);
-        Socket connection = mysocket.accept();
+        while (true){
+            ServerSocket mysocket = new ServerSocket(Integer.parseInt(portField.getText()));
+            System.out.println("Server is waiting for a client!");
+            stage.setScene(gameInfoScene);
+            Socket connection = mysocket.accept();
+            ClientThread c = new ClientThread(mysocket.accept(), count);
+//
+            clientThreads.add(c);
+            c.start();
 
-        System.out.println("Server has a client!!!");
-        System.out.println("This is the remote port the client is using: " + connection.getPort());
+            count ++;
+        }
 
-        ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
-        connection.setTcpNoDelay(true);
 
     }
 
@@ -64,22 +67,39 @@ public class ServerController {
     }
 
     public void updateClientList(ArrayList<String> clients) {
-
+        for(ClientThread c : clientThreads){
+            System.out.println(c.count);
+            System.out.println(c.isAlive());
+        }
     }
 
 
     public class ClientThread extends Thread {
         Socket connection;
         ObjectInputStream inputStream;
-        ObjectInputStream objectInputStream;
+        ObjectOutputStream outputStream;
         int count;
 
         ClientThread(Socket connection,int count){
             this.connection = connection;
             this.count = count;
         }
-        
+
+        @Override
+        public void run() {
+            try{
+                inputStream = new ObjectInputStream(connection.getInputStream());
+                outputStream = new ObjectOutputStream(connection.getOutputStream());
+                connection.setTcpNoDelay(true);
+                System.out.println("Server has a client!!!");
+                System.out.println("This is the remote port the client is using: " + connection.getPort());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
 
 
+
+
+        }
     }
 }
